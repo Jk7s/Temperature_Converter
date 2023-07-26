6245dee06aa567414e8198c34236a2b8f772148a
Sravani Koyyala <112328653+Jk7s@users.noreply.github.com>
2023-07-26 10:25:48 +0530

    Add files via upload

---
+++ tp2.js
@@ -0,0 +1,26 @@
+const celsiusElem = document.querySelector("#celsius");
+const degree = document.querySelector("#degree");
+const convertBtn = document.querySelector("#convert-btn");
+const tempType = document.querySelector("#temp-type");
+
+window.addEventListener("load", () => {
+  degree.value = "";
+  celsiusElem.innerHTML = "";
+});
+
+convertBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  convertToCelsius();
+});
+
+function convertToCelsius() {
+  let inputValue = degree.value;
+
+if (tempType.value === "Celsius") {
+    const FahrenheitToCelsius = (inputValue - 32) * (5 / 9);
+    celsiusElem.innerHTML = `${FahrenheitToCelsius.toFixed(3)} &deg;C`;
+  } else if (tempType.value === "fahrenheit") {
+    const CelsiusToFahrenheit = inputValue *(9/5) +32;
+    celsiusElem.innerHTML = `${CelsiusToFahrenheit.toFixed(3)} &deg;F`;
+  }
+}
